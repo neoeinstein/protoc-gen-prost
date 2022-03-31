0e23a899e6d3cd6a3c1d3d1bee67cb0d384de40f
Marcus Griep <marcus@griep.us>
2022-03-31 12:01:13 -0400

    docs: show example of extending a same-crate generated type

--- example/build-with-buf/src/lib.rs
+++ example/build-with-buf/src/lib.rs
@@ -1,1 +1,9 @@
 include!("gen/mod.rs");
+
+// Types within the same crate can be extended with your own impl blocks like so:
+#[cfg(feature = "example")]
+impl example::ExampleRequest {
+    pub fn borrow_request_value(&self) -> &str {
+        &self.example
+    }
+}
